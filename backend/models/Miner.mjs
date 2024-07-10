31722135e795dcf20c54b7192e81c59ce4a9ee66
Christian Lopez <chrisdev92@outlook.com>
2024-07-10 18:47:09 +0200

    Miner class added minetransaction method

--- backend/models/Miner.mjs
+++ backend/models/Miner.mjs
@@ -9,23 +9,16 @@ export default class Miner {
   }
 
   mineTransaction() {
-    console.log("Pending Transactions", this.transactionPool.pendingTransactions);
    const validTransactions = this.transactionPool.validateTransactions();
-   console.log("Valid Transactions", validTransactions);
-    //validTransactions.push(Transaction.rewardTransaction({ miner: this.wallet }) );
     if(this.blockchain.chain.length > 1) {
       const rewardTransaction = Transaction.rewardTransaction({miner: this.wallet,});
       validTransactions.push(rewardTransaction);
  }
  
     this.blockchain.addBlock({ data: validTransactions });
- console.log("Transactions to be added to the block", validTransactions);   
     this.pubsub.broadcast();
     this.transactionPool.emptyTransactions();
     
-    //const rewardTransaction = Transaction.rewardTransaction({miner: this.wallet,});
-    //this.transactionPool.addTransaction(rewardTransaction);
-
 if (this.blockchain.chain.length > 0) {
   const rewardTransaction = Transaction.rewardTransaction({
     miner: this.wallet,
