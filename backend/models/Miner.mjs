99cdd711d80120647efe633f13c9bfa7172e62ad
Christian Lopez <chrisdev92@outlook.com>
2024-07-10 18:38:35 +0200

    Finished the Miner class and implemented miningtransaction logic

--- backend/models/Miner.mjs
+++ backend/models/Miner.mjs
@@ -9,10 +9,31 @@ export default class Miner {
   }
 
   mineTransaction() {
-    const validTransactions = this.transactionPool.validateTransactions();
-    validTransactions.push(Transaction.rewardTransaction({ miner: this.wallet }) );
+    console.log("Pending Transactions", this.transactionPool.pendingTransactions);
+   const validTransactions = this.transactionPool.validateTransactions();
+   console.log("Valid Transactions", validTransactions);
+    //validTransactions.push(Transaction.rewardTransaction({ miner: this.wallet }) );
+    if(this.blockchain.chain.length > 1) {
+      const rewardTransaction = Transaction.rewardTransaction({miner: this.wallet,});
+      validTransactions.push(rewardTransaction);
+ }
+ 
     this.blockchain.addBlock({ data: validTransactions });
+ console.log("Transactions to be added to the block", validTransactions);   
     this.pubsub.broadcast();
     this.transactionPool.emptyTransactions();
+    
+    //const rewardTransaction = Transaction.rewardTransaction({miner: this.wallet,});
+    //this.transactionPool.addTransaction(rewardTransaction);
+
+if (this.blockchain.chain.length > 0) {
+  const rewardTransaction = Transaction.rewardTransaction({
+    miner: this.wallet,
+  });
+  this.transactionPool.addTransaction(rewardTransaction);
+  this.pubsub.broadcastTransaction(rewardTransaction);
+
+}
+
   }
 }
