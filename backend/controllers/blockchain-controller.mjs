9a246b7db698f9e2259e3d85dd53eca591cb06f2
Christian Lopez <chrisdev92@outlook.com>
2024-07-10 18:26:12 +0200

    endpoint for blockchain and blockchainMongodb added

--- backend/controllers/blockchain-controller.mjs
+++ backend/controllers/blockchain-controller.mjs
@@ -1,8 +1,39 @@
 import { blockchain } from "../server.mjs";
+import BlockchainModel from '../models/BlockchainMongooseModel.mjs';
 
-export const getBlockchain = (req, res) => { 
 
-const mainChain = blockchain.chain;
+export const getBlockchain = async (req, res) => { 
+    
+res.status(200).json({success: true, statusCode: 200, data: blockchain.chain})
+}
+  
 
-    res.status(200).json({success: true, statusCode: 200, data: mainChain})
+
+export const getMongoBlockchain = async (req, res) => { 
+
+     try {
+       let existingBlockchain = await BlockchainModel.findOneAndUpdate();
+       if (existingBlockchain) {
+         existingBlockchain.chain = blockchain.chain;
+         await existingBlockchain.save();
+       } else {
+         const newBlockchain = new BlockchainModel({ chain: blockchain.chain });
+         await newBlockchain.save();
+       }
+
+       console.log('BLOCKCHAIN UPDATED IN MONGODB:', blockchain.chain);
+
+       res.status(200).json({
+         success: true,
+         statusCode: 200,
+         data: existingBlockchain.chain,
+       });
+     } catch (error) {
+       console.error('Failed to update blockchain in MongoDB:', error);
+       res.status(500).json({
+         success: false,
+         statusCode: 500,
+         message: 'Failed to update blockchain in MongoDB',
+       });
+     }
 }
