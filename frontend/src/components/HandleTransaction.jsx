c18f09de20b1320b69b55fbf604557b16f534586
Christian Lopez <chrisdev92@outlook.com>
2024-07-13 09:35:29 +0200

    post request and sending the token from localstorage

---
+++ frontend/src/components/HandleTransaction.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Transaction } from '../pages/Transaction';
+
+const HandleTransaction = () => {
+      const transactionToMake = async (transactionData) => {
+
+        const token = localStorage.getItem("token");
+        console.log("Token retreived from localstorage In handleTransaction:", token);
+        try {
+              const response = await fetch(
+                'http://localhost:4001/api/v1/wallet/transaction',
+                {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                  },
+                  body: JSON.stringify(transactionData),
+                }
+              );
+
+              const data = await response.json();
+
+              return data;
+        } catch (error) {
+            console.error('Transaction failed:', error);
+        }
+      
+      };
+  return (
+    <div>
+      <Transaction transactionToMake={transactionToMake} />
+    </div>
+  )
+}
+
+export default HandleTransaction
