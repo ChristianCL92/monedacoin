3a68163179acf7aa8c88375956732d2ae85c1488
Christian Lopez <chrisdev92@outlook.com>
2024-07-18 22:24:26 +0200

    block condition added to controller

--- backend/controllers/block-controller.mjs
+++ backend/controllers/block-controller.mjs
@@ -7,7 +7,9 @@ export const mineBlock = (req, res) => {
     const data = req.body;
         
     const block = blockchain.addBlock({data : data});
-    
+    if(!block) {
+        return res.status(400).json({success: false, statusCode: 400, error: "Failed to mine block"})
+    }
     pubnubServer.broadcast();
 
     res.status(201).json({success: true, statusCode: 201, data: block})
