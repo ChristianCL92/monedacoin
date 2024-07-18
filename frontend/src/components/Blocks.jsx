3e26179258e7a40ac861a6176621de792cd4b451
Christian Lopez <chrisdev92@outlook.com>
2024-07-18 18:03:53 +0200

    Updated the blocks component transaction data

--- frontend/src/components/Blocks.jsx
+++ frontend/src/components/Blocks.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import TransactionData from './TransactionData'
+import "../styles/App.css"
 
 const Blocks = ({block}) => {
   return (
     <>
-
+      <div className='block'>
+        <h3>Block:{block.index}</h3>
+        <p>Timestamp:{new Date(block.timestamp).toLocaleTimeString()}</p>
+        <p>Last Hash:{block.lastHash.slice(0, 15)}...</p>
+        <p>Hash:{block.hash.slice(0, 15)}...</p>
+        <p>Nonce:{block.nonce}</p>
+        <p>Difficulty:{block.difficulty}</p>
+        <h5>Transaction data</h5>
+        {block.data.map((transaction, index) => (
+          < TransactionData key={index} transaction={transaction}/>
+        ))}
+      </div>
     </>
   )
 }
