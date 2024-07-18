8196f529575696efbc37a017f7ec44cb8e11ebac
Christian Lopez <chrisdev92@outlook.com>
2024-07-18 17:47:22 +0200

    styling the blockchain

---
+++ frontend/src/components/TransactionData.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../styles/App.css"
+
+const TransactionData = ({transaction}) => {
+
+  const { inputMap, outputMap } = transaction;
+  const outputKeys = Object.keys(outputMap);
+  const outputValues = Object.values(outputMap);
+  return (
+    <>
+      <div className="transaction">
+        <h6>Transaction ID: {transaction.id}</h6>
+        <p>Recipient:{outputKeys[0].slice(0, 30)}...</p>
+        <p>Sent:{outputValues[0]}</p>
+        {inputMap.amount && <p>Initial Balance:{inputMap.amount}</p>}
+
+        <p>Sender Address: {inputMap.address.slice(0, 30)}...</p>
+      </div>   
+    </>
+  );
+}
+
+export default TransactionData
