a8f63f271f16aa8a2242678bdac0ec9d6fb7a1f6
Christian Lopez <chrisdev92@outlook.com>
2024-07-18 22:50:01 +0200

    added conditional execution for displaying either reward or sent jsx

--- frontend/src/components/TransactionData.jsx
+++ frontend/src/components/TransactionData.jsx
@@ -11,8 +11,12 @@ const TransactionData = ({transaction}) => {
       <div className="transaction">
         <h6>Transaction ID: {transaction.id}</h6>
         <p>Recipient:{outputKeys[0].slice(0, 30)}...</p>
-        <p>Sent:{outputValues[0]}</p>
-        
+{transaction.inputMap.address === "authorized-reward-address" ? (
+    <p>Reward amount: {outputValues[0]}</p>
+) : (
+    <p>Sent: {outputValues[0]}</p>
+)}
+
         {inputMap.amount && <p>Initial Balance:{inputMap.amount}</p>}
         <p>Sender Address: {inputMap.address.slice(0, 30)}...</p>
       </div>   
